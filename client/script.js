85f400fdb886edbec6c55b3714d6d2a5b3ad73f4
[Hithesh N <[hitheshsrv@gmail.com]>
2024-07-15 19:01:16 +0530

    adding required files

--- client/script.js
+++ client/script.js
@@ -42,3 +42,11 @@ body.addEventListener("click" , e =>{
     }
 });
 
+var typed=new Typed(".text",{
+  strings:["Frontend Developer", "Web Developer"],
+  typeSpeed:100,
+  backSpeed:50,
+  backDelay:1000,
+  loop:true
+})
+
